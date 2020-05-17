581ea222ecb9e475e20f16609b1d1e9fa962ab0a
tommywu10 <tomwu10@gmail.com>
2020-05-17 11:34:43 -0400

    wwwww

--- app.js
+++ app.js
@@ -12,3 +12,13 @@ function carousel() {
   x[myIndex-1].style.display = "block";
   setTimeout(carousel, 3000)
 }
+
+function initMap() {
+  // The location of Uluru
+  var longmont = {lat: 40.10, lng: -105.109};
+  // The map, centered at Uluru
+  var map = new google.maps.Map(
+      document.getElementById('map'), {zoom: 10, center: longmont});
+  // The marker, positioned at Uluru
+  var marker = new google.maps.Marker({position: longmont, map: map});
+}
